e9b024fbac4b0a691d03eb16159c88681f64a3d2
lucasyoungers <lucas005xl@gmail.com>
2024-09-19 15:50:49 -0700

    slight refactor

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
 #!/usr/bin/env node
 
-const url = "https://pokeapi.co/api/v2/pokemon/"
-
-async function getNumber(speciesUrl) {
-    const res = await fetch(speciesUrl)
+async function fetchData(url) {
+    const res = await fetch(url)
     if (!res.ok) {
         console.error(res.statusText)
-        return
+        exit(1)
     }
-    const data = await res.json()
-    return data.pokedex_numbers.find(o => o.pokedex.name === "national").entry_number
+    return res.json()
 }
 
-async function getSpecies(speciesUrl) {
-    const res = await fetch(speciesUrl)
-    if (!res.ok) {
-        console.error(res.statusText)
-        return
-    }
-    const data = await res.json()
-    return data.genera.find(o => o.language.name === "en").genus
+function getNumber(speciesData) {
+    return speciesData.pokedex_numbers.find(o => o.pokedex.name === "national").entry_number
 }
 
-function getFtInHeight(dmHeight) {
-    const inches = dmHeight * 3.937008
+function getSpecies(speciesData) {
+    return speciesData.genera.find(o => o.language.name === "en").genus
+}
+
+function getHeight(pokemonData) {
+    const inches = pokemonData.height * 3.937008
     const ft = Math.floor(inches / 12)
     const _in = Math.round(inches % 12)
     return { ft, in: _in }
 }
 
-function getLbWeight(hgWeight) {
-    const weight = hgWeight * 0.2204623
+function getWeight(pokemonData) {
+    const weight = pokemonData.weight * 0.2204623
     return Math.round(weight * 10) / 10
 }
 
 async function main() {
+    // get pokémon from user
     if (process.argv.length !== 3) {
         console.error("Usage: pkmn-dex-string <pokémon name>")
-        return
+        exit(1)
     }
-    const mon = process.argv[2].toLowerCase()
+    const pokemon = process.argv[2].toLowerCase()
 
-    const res = await fetch(url + mon)
-    if (!res.ok) {
-        console.error(res.statusText)
-        return
-    }
-    const data = await res.json()
+    // get data from api
+    const pokemonData = await fetchData("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+    const speciesData = await fetchData(pokemonData.species.url)
 
-    const number = await getNumber(data.species.url)
-    const species = await getSpecies(data.species.url)
-    const height = getFtInHeight(data.height)
-    const weight = getLbWeight(data.weight)
+    // process data
+    const number = getNumber(speciesData)
+    const species = getSpecies(speciesData)
+    const height = getHeight(pokemonData)
+    const weight = getWeight(pokemonData)
 
+    // convert data to string format
     const svFormat = `NO. ${String(number).padStart(4, "0")}  ${species}  HT: ${height.ft}'${height.in}"  WT: ${weight} lbs.`
     console.log(svFormat)
 }
